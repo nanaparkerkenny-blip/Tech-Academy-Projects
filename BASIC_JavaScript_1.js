e9d9a79908842f689cc6eb2fe3508a2795410909
nanaparkerkenny-blip <nanaparkerkenny@gmail.com>
2026-04-13 11:44:32 +0100

    Update BASIC_JavaScript_1.js

--- BASIC_JavaScript_1.js
+++ BASIC_JavaScript_1.js
@@ -1,1 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <body>
+  <!-- The alert method is deleted from here and replaced by the link below-->
 
+  <script src="Basic_JavaScript_1.js"></script>
+  </body>
+  </html>
